9aab4f40f9e1a321257a473625ff4c9cb09611b2
Aviral Singh <aviralsingh3501@gmail.com>
2024-01-14 16:52:33 +0530

    Uisng bodyParser

--- current_week/http-server-01.js
+++ current_week/http-server-01.js
@@ -1,6 +1,13 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const port =3000;
+
+app.use(bodyParser.json());
+
+app.listen(port ,()=>{
+    console.log(`Server is running on ${port}`);
+})
 
 app.get("/",(req,res)=>{
     res.send("Hello World - landing page ")
@@ -8,3 +15,13 @@ app.get("/",(req,res)=>{
 app.get("/home",(req,res)=>{
     res.send("Home page");
 })
+app.post('/post', (req, res) => {
+    // Process the data here
+    //res.send("<b>Post request recieved</b>");
+    //or
+    const message = req.body.message ;
+    res.json({
+        ouptut : "Zoro",
+        sanji : "number 7 "
+    })
+});
