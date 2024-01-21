a54b1ff877486272eb9bcfa92a567bb0e262e12a
Aviral Singh <aviralsingh3501@gmail.com>
2024-01-21 13:08:37 +0530

    why we use middlewares

--- Middlewares/first.js
+++ Middlewares/first.js
@@ -7,7 +7,7 @@ app.listen(port , ()=>{
     console.log(`App is listening on port number ${port}`);
 })
 
-// here we are considerung only one user 
+// here we are considering only one user 
 app.get("/",(req,res)=>{
     //do health checks here 
     const kidneyId = req.query.kidneyId;
@@ -38,3 +38,35 @@ app.get("/",(req,res)=>{
 
 //http://localhost:3000/?kidneyId=1
 //http://localhost:3000/?kidneyId=3
+
+//now lets say we have to create another route --> we have to do the same auth and input checks multiple times
+//DO NOT REPEAT YOURSELF (DRY)
+// hence for that we use middlewares 
+
+//eg
+app.get("/user",(req,res)=>{
+    //do health checks here 
+    const kidneyId = req.query.kidneyId;
+    const username = req.headers.username;
+    const password = req.headers.password;
+
+    //Authentication of user 
+    if(username!="harirat" && password!="pass"){
+        res.status(403).json({
+            msg:"user does not exist,"
+        });
+        return;
+    }
+
+    //input validation
+    if(kidneyId!=1 && kidneyId!=2){
+        res.status(411).json({
+            msg: "wrong inputs",
+        });
+        return;
+    }
+    //do somthing
+    res.send("Your Information is as follows ");
+})
+
+// to avoid repeating we use Middlewares 
