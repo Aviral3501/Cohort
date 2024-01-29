46e00b14684eb2335b754f8c01173e3ccfbf6fdc
Aviral Singh <aviralsingh3501@gmail.com>
2024-01-29 20:45:10 +0530

    Custom Reusable Button Component

--- firstProjectVite-React/src/App.jsx
+++ firstProjectVite-React/src/App.jsx
@@ -1,19 +1,27 @@
 import { useState } from 'react'
 
 function App() {
+
   const [count , setCount] = useState(0);
-
-  function onClickHandler(){
-    setCount(count+1);
-  }
-
   return (
     <>
         <div>
-          <button onClick={onClickHandler}> Count is {count} </button>
+          <CustomButton count={count} setCount={setCount}></CustomButton>
         </div>
     </>
   )
 }
+
+//component button
+function CustomButton(props){
+
+    function onClickHandler(){
+      props.setCount(props.count+1);
+      // console.log(props.count);
+    }
+
+  return <button onClick={onClickHandler}>Count is {props.count}</button>
+}
+
 
 export default App
