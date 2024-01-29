83bef84f47ffa31642e21d9cc27491cc9ea320d2
Aviral Singh <aviralsingh3501@gmail.com>
2024-01-29 09:57:10 +0530

    Psuedo rect working using simple button counter

---
+++ firstProjectVite-React/PseudoReact/index.js
@@ -0,0 +1,28 @@
+let state = {
+    count :0
+}
+
+//updates the value of the counter
+//state equivalent
+function onButtonPress(){
+    state.count++;
+    buttonComponentRender();
+}
+
+//rendering functionality of react
+function buttonComponentRender(){
+    document.getElementById("buttonParent").innerHTML = "";
+    const component = buttonComponent(state.count);
+    document.getElementById("buttonParent").appendChild(component);
+}
+
+//creates a button updates the value of the counter 
+//button component
+function buttonComponent(count){
+    const button = document.createElement("button");
+    button.innerHTML = `Counter ${count}`;
+    button.setAttribute("onClick" , `onButtonPress()`);
+    return button;
+}
+
+buttonComponentRender();
