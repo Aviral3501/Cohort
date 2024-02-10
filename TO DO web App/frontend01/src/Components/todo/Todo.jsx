2fde83c2ac8165ba897166cbc2cc8dd75a6d2617
Aviral Singh <aviralsingh3501@gmail.com>
2024-02-10 17:33:35 +0530

    todo component created

--- TO DO web App/frontend01/src/Components/todo/Todo.jsx
+++ TO DO web App/frontend01/src/Components/todo/Todo.jsx
@@ -1,11 +1,90 @@
-import React from 'react'
+import React, { useState } from 'react';
+import "./todo.css";
+import TodoCards from './TodoCards';
 
 const Todo = () => {
+
+    const [ Inputs , setInputs] = useState({
+        title:"",
+        description:""
+    })
+
+    //to store the list of todos
+
+    const [Array , setArray] = useState([]);
+
+    //to show description only when the user clicks on the title
+    const show = () =>{
+        document.getElementById("textarea").setAttribute("style", "display:block");
+    }
+    
+    //on add todo , add the input to the original todos
+    const change = (e) =>{
+        const {name,value}= e.target;
+        setInputs({
+            ...Inputs,
+            [name]:value
+        });
+
+    }
+
+    //rerendering on submitting the todo -> empty the inputs for the next input and add the current element to the array
+    const submit = ()=>{
+        setArray([...Array , Inputs]);
+        setInputs({title:"", description:""});
+        
+    }
+
+
   return (
-    <div>
-      
+    <div className="todo">
+        <div className="todo-main container d-flex justify-content-center align-items-center flex-column" >
+            <div className='d-flex flex-column todo-inputs-div w-50 p-1 '>
+                <input 
+                className="my-2 p-2 todo-inputs"
+                 type="text" placeholder='TITLE
+                 ' name="title" onClick={show}
+                 value={Inputs.title}
+                  onChange={change} >
+
+                  </input>
+
+
+                <textarea 
+                id="textarea" 
+                className="my-2 p-2 todo-inputs"
+                 type="text" 
+                 placeholder='DESCRIPTION'
+                  name="description"
+                  value={Inputs.description}
+                  onChange={change}>
+
+                  </textarea>
+               
+            </div>
+            <div className='w-50 my-3 d-flex justify-content-end'>
+            <button className="home-btn px-2 py-1" onClick={submit}>Add</button>
+            </div>
+            
+               
+        </div>
+        <div className="todo-body">
+            <div className="container-fluid">
+                <div className='row '>
+                    {Array && Array.map((item , index) =>(
+                        <>
+                        <div className='col-lg-3  mx-5 my-2'>
+                            <TodoCards/>
+                        </div> 
+                        </>
+                    ))}
+                </div>
+               
+            </div>
+        </div>
+
     </div>
   )
 }
 
-export default Todo
+export default Todo;
---
+++ TO DO web App/frontend01/src/Components/todo/TodoCards.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const TodoCards = (title,description) => {
+  return (
+    <div className='p-3 todo-card'>  
+    <h5>Heading</h5>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit culpa sed fuga officiis, maxime placeat tempora. Velit, molestiae architecto! Eos assumenda ipsum voluptates tenetur ab sequi suscipit eum obcaecati.</p>
+    </div>
+  )
+}
+
+export default TodoCards;
