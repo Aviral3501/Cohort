6fd30fb2ad05b7544096024b5a675ab744d1e2c0
Aviral Singh <aviralsingh3501@gmail.com>
2024-02-12 10:57:37 +0530

    close button added

--- TO DO web App/frontend01/src/Components/todo/Update.jsx
+++ TO DO web App/frontend01/src/Components/todo/Update.jsx
@@ -6,7 +6,12 @@ const Update = () => {
         <h3>Update your task</h3>
         <input className="todo-inputs my-4 w-100 p-3" type='text' placeholder='New Title' ></input>
         <textarea className='todo-inputs w-100 p-3 ' placeholder='New Description'></textarea>
-        <button className='btn btn-dark my-4'>UPDATE</button>
+        <div className='w-100  d-flex justify-content-start'>
+                <button className='btn btn-dark my-4'>UPDATE</button>
+                <button className='btn btn-danger my-4 mx-3'>Close</button>
+        </div>
+        
+
       
     </div>
   )
