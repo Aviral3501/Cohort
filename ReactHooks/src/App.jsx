5af75482cbc653f692dc3d99b05707b2c0b4bffd
Aviral Singh <aviralsingh3501@gmail.com>
2024-02-17 17:24:32 +0530

    leaning routing

---
+++ ReactHooks/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import router from "react-router-dom";
+const { Link } = router;
+
+//routing - client side
+
+const App = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default App
