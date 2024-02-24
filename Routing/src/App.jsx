557675f32921fcf2b5b276b242d6466c26f39389
Aviral Singh <aviralsingh3501@gmail.com>
2024-02-24 14:10:54 +0530

    context Api

--- Routing/src/App.jsx
+++ Routing/src/App.jsx
@@ -1,34 +1,50 @@
-import React, { Suspense } from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-
-const Dashboard = React.lazy(() => import("./Components/Dashboard"));
-const Landing = React.lazy(() => import("./Components/Landing"));
-
-import Navbar from './Components/Navbar';
+import React, { useContext } from 'react'
+import { useState } from 'react'
+import { CountContext } from './context';
 
 const App = () => {
+  const [count ,setCount]=useState(0);
+
+  //wrap anyone that wants to use the teleported value inside a provider
   return (
-    <BrowserRouter>
-      <div>
-        <Navbar />
+    <div>
+      <CountContext.Provider value={count}>
+      <Count  setCount={setCount}/>
+      </CountContext.Provider>
+     
+    </div>
+  )
+}
 
-        <div>
-          <Routes>
-            <Route path="/dashboard" element={<Suspense fallback={"...loading"}><Dashboard /></Suspense>} />
-            {/* why suspense --> in lazy routing , it takes time to fetch the bundle (may be 2s or 10s depends on net speed) 
-            hence we use suspense api , which is used for async data fetching , while it loads it will show ...loading */}
-            <Route path="/landing" element={<Suspense fallback={"...loading"}><Landing/></Suspense>} />
-          </Routes>
-        </div>
-      </div>
-    </BrowserRouter>
-  );
-};
+const Count  = ({setCount}) =>{
+  return (
+    <>
+    <CountRenderer ></CountRenderer>
+    <Buttons  setCount={setCount}/>
+      
+    </>
+  )
+}
 
-export default App;
+const CountRenderer =() =>{
+  const count=useContext(CountContext);
+  return (
+    <>
+    {count}
+    </>
+  )
+} 
+const Buttons = ({setCount}) =>{
+  const count=useContext(CountContext);
+  return(
+    <>
+    <br/>
+    <button onClick={()=>{setCount(count+1)}}>Increase</button>
+    <br/>
+    <button onClick={()=>{setCount(count-1)}}>Decrease</button>
+    </>
+  )
 
-//using lazy loading
-// in normal or client side loading -- whole client bundle is geven at once 
-// , and with routing we tarverse different pages
-// however in lazy loading, we only give the bundle , when asked for or when needed 
-//there is no need to guve the whole bundle at once but rather in parsts when needed
+}
+
+export default App
---
+++ Routing/src/context.jsx
@@ -0,0 +1,3 @@
+import { createContext } from "react";
+export const CountContext =createContext(0);
+//this is the teleported which will teleport count variable 
