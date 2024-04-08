b3a85b3abe05f3817268c354a3c07b07c3c5cc42
Aviral Singh <aviralsingh3501@gmail.com>
2024-04-08 14:39:58 +0530

    added loading functionality to the hook

--- Week09/ReactCustomHooks/CustomHooks/CustomDataFetchinfHook.jsx
+++ Week09/ReactCustomHooks/CustomHooks/CustomDataFetchinfHook.jsx
@@ -2,23 +2,36 @@ import { useEffect, useState } from 'react'
 import axios from 'axios'
 
 
-// why useCustom hooks ---> to make your code more cleaner and resusable
+// why useCustom hooks ---> to make your code more cleaner and
 
 function useTodos() {
+
   const [todos, setTodos] = useState([])
+  const [loading,setLoading]=useState(true);
 
   useEffect(() => {
+    // setLoading(true);
     axios.get("https://sum-server.100xdevs.com/todos")
       .then(res => {
         setTodos(res.data.todos);
+        setLoading(false);
       })
+     
   }, [])
 
-  return todos;
+ 
+
+  return {todos,loading};
 }
 
 function CustomDataFetching() {
-  const todos = useTodos();  //using the custom hooks to fetch the data from the server
+  const {todos,loading} = useTodos();  //using the custom hooks to fetch the data from the server
+
+  if(loading){
+    return(
+      <div>loading...</div>
+    )
+  }
 
   return (
     <>
