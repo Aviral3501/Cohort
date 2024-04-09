31466303e85be82d188e1975e2e80fce3d80c9e1
Aviral Singh <aviralsingh3501@gmail.com>
2024-04-09 08:42:32 +0530

    client side basics done

--- Week14/fistnextapp/app/signin/layout.tsx
+++ Week14/fistnextapp/app/signin/layout.tsx
@@ -3,12 +3,12 @@ export default function({children}:{children:React.ReactNode}){
 
     return(
         <>
-        <div className="border-b p-1 text-red-600 bg-neutral-500/85 hover:bg-neutral-100" >
+        <div className="border-b p-1 mt-4 text-center text-red-600 bg-neutral-500/85 hover:bg-neutral-100" >
             20% off for the next few days! Use code OFF50 at checkout , HURRY UP
         </div>
         <div>
             {children}
         </div>
         </>
-    )
+    ) 
 }
