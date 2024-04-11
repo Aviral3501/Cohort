d432d9855f21aa635a706454653fbc95598f18b0
Aviral Singh <aviralsingh3501@gmail.com>
2024-04-11 14:12:54 +0530

    useRouter in next done

--- Week14/nextbackend/app/api/user/route.ts
+++ Week14/nextbackend/app/api/user/route.ts
@@ -1,4 +1,28 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { PrismaClient } from '@prisma/client';
+const client = new PrismaClient();
 
+
+// get request which return json data
 export async function GET() {
     return Response.json({ name: "Aviral", email: "aviralsingh@gmail.com" })
   }
+
+
+
+
+
+  export async function POST(req: NextRequest) {
+    const body = await req.json();
+    // should add zod validation here
+    const user = await client.user.create({
+        data: {
+            username: body.username,
+            password: body.password
+        }
+    });
+
+    console.log(user.id);
+
+    return NextResponse.json({ message: "Signed up" });
+}
